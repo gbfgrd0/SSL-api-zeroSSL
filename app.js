8fa977278aaab026baa598a991f2cfdda72b8280
Gabriel Figueiredo <118404050+gbfgrd0@users.noreply.github.com>
2023-07-10 17:03:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-app.get('/.well-known/pki-validation/0C583F7B328B7F4A39F7518BF379F4EB.txt', (req, res) => {
-  const filePath = path.join(__dirname, '0C583F7B328B7F4A39F7518BF379F4EB.txt');
+app.get('/.well-known/pki-validation/<arquivo>.txt', (req, res) => {
+  const filePath = path.join(__dirname, '<arquivo>.txt');
   res.sendFile(filePath);
 });
 
 app.listen(port, () => console.log(`Servidor aberto na porta: ${port}`));
+
+// Obs: o arquivo de texto para a confirmação deve estar no mesmo diretório
